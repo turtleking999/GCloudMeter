7ebc08114963c73cc37fc6d2c24f003a52fbb3b9
weihao <turtleking999@gmail.com>
2017-06-07 21:49:15 +0800

    meteor

---
+++ client/main.js
@@ -0,0 +1,6 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+import './imports/myapp/initApp/ainitapp.js'
+import './imports/myapp/createAccount/createAccount.js';
